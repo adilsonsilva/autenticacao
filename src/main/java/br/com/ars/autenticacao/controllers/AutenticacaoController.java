9ed7fdeeaf8e93b6d0f5c422e32a4a83cb3bd859
= <adilson.ro.silva@gmail.com>
2019-02-09 16:00:44 -0200

    Configuração log4j2

--- src/main/java/br/com/ars/autenticacao/controllers/AutenticacaoController.java
+++ src/main/java/br/com/ars/autenticacao/controllers/AutenticacaoController.java
@@ -1,14 +1,18 @@
 package br.com.ars.autenticacao.controllers;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 @RestController
 public class AutenticacaoController {
+
+	private static Logger LOGGER = LogManager.getLogger(AutenticacaoController.class);
 
 	@GetMapping("v1/teste/{teste}")
 	public ResponseEntity<String> teste(@RequestParam String teste) {
@@ -17,8 +21,10 @@ public class AutenticacaoController {
 
 	@GetMapping("v1/nome")
 	public ResponseEntity<Pessoa> nome() {
+
+		LOGGER.info("ACESSO AO METODO GET NOME");
 		
-		Pessoa pessoa = new Pessoa("Adilson","Silva");
+		Pessoa pessoa = new Pessoa("Adilson", "Silva");
 
 		return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
 	}
